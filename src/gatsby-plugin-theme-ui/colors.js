8ad01e2f1b4f18947cfb6e0166872c11db82ca2d
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-21 00:17:35 +0530

    minor changes in the theme

--- src/gatsby-plugin-theme-ui/colors.js
+++ src/gatsby-plugin-theme-ui/colors.js
@@ -1,7 +1,7 @@
 export default {
   text: "#000",
   background: "#fff",
-  primary: "#e63b19",
+  primary: "#ee4c2c",
   secondary: "#c70d3a",
   sidebar: "#eee",
   borderColor: "rgba(0, 0, 0, 0.15)",
@@ -9,7 +9,7 @@ export default {
     dark: {
       text: "#fff",
       background: "#182952",
-      primary: "#f638dc",
+      primary: "#ee4c2c",
       secondary: "#ff7976",
       sidebar: "#101d3c",
       borderColor: "rgba(255, 255, 255, 0.15)"
@@ -17,7 +17,7 @@ export default {
     cool: {
       text: "#fff",
       background: "#05386b",
-      primary: "#5cdb95",
+      primary: "#ee4c2c",
       secondary: "#bef992",
       sidebar: "#052e56",
       borderColor: "rgba(255, 255, 255, 0.15)"
@@ -25,7 +25,7 @@ export default {
     deep: {
       text: "#fff",
       background: "hsl(230,25%,18%)",
-      primary: "hsl(260, 100%, 80%)",
+      primary: "#ee4c2c",
       secondary: "hsl(290, 100%, 80%)",
       sidebar: "hsla(230, 20%, 0%, 20%)",
       borderColor: "rgba(255, 255, 255, 0.15)"
