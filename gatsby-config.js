383659a7d42736546da03d2de0381f3626bd48d6
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-21 21:59:45 +0530

    updated the codebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ module.exports = {
   siteMetadata: {
     title: `Document by Code Bushi`,
     name: `Code Bushi`,
-    siteUrl: `https://gatsby-theme-document.netlify.com`,
+    siteUrl: `https://codebushi.com`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     social: [
       {
         name: `github`,
-        url: `https://github.com/codebushi/gatsby-theme-document`
+        url: `https://github.com/codebushi`
       },
       {
         name: `twitter`,
@@ -15,9 +15,54 @@ module.exports = {
       }
     ],
     sidebarConfig: {
-      forcedNavOrder: ["/introduction", "/codeblock"],
-      ignoreIndex: true
+      forcedNavOrder: ['/'],
+      ignoreIndex: false
     }
   },
-  plugins: [{ resolve: `gatsby-theme-document` }]
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `content`,
+        name: `content`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 704
+            }
+          },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              icon: false
+            }
+          },
+          `gatsby-remark-embed-video`
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Document`,
+        short_name: `Document`,
+        start_url: `/`,
+        background_color: `#182952`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'src/site-icon.png'
+      }
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-theme-ui`,
+    `gatsby-plugin-react-helmet`
+  ]
 };
---
+++ src/templates/docs.js
@@ -0,0 +1,40 @@
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
+const DocsTemplate = ({ data, location }) => {
+  const { mdx } = data;
+
+  return (
+    <Layout tableOfContents={mdx.tableOfContents} location={location}>
+      <SEO title={mdx.frontmatter.title} description={mdx.frontmatter.description} />
+      {/* <Heading>{mdx.frontmatter.title}</Heading> */}
+      <MDXRenderer>{mdx.body}</MDXRenderer>
+    </Layout>
+  );
+};
+
+DocsTemplate.propTypes = {
+  data: PropTypes.shape({
+    mdx: PropTypes.object.isRequired
+  }).isRequired,
+  location: PropTypes.object.isRequired
+};
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    mdx(fields: { id: { eq: $id } }) {
+      body
+      tableOfContents
+      frontmatter {
+        title
+        description
+      }
+    }
+  }
+`;
+
+export default DocsTemplate;
